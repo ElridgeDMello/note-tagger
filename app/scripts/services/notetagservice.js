dbd23ef340f36810653b52d6954b51cb37ef319f
Elridge D'Mello <elridge.dmello@drillinginfo.com>
2015-05-19 23:45:24 -0500

    Define coarse API for noteTagService

--- app/scripts/services/notetagservice.js
+++ app/scripts/services/notetagservice.js
@@ -10,6 +10,22 @@
 angular.module('noteTaggerApp')
   .service('noteTagService', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
-    this.addTags = function() {};
+
+    this.addTag = function(/*noteId, tag*/) {};
+
+    this.addTags = function(/*noteId, tags*/) {};
+
+    // function to give state
+    this.getAssociatedNotes = function(/*tag*/) {
+
+    };
+
+    this.getActiveNotes = function() {
+
+    };
+
+    this.activateTags = function(/*tags*/) {
+
+    };
 
   });
--- test/spec/services/notetagservice.js
+++ test/spec/services/notetagservice.js
@@ -11,15 +11,36 @@ describe('Service: noteTagService', function () {
     noteTagService = _noteTagService_;
   }));
 
-  it('should do something', function () {
-    expect(!!noteTagService).toBe(true);
+  describe('the addTag method', function() {
+    it('is defined', function() {
+      expect(noteTagService.addTag).toBeFunction();
+    });
   });
 
   describe('the addTags method', function() {
 
     it('is defined', function() {
-      expect(noteTagService.addTags).toBeDefined();
+      expect(noteTagService.addTags).toBeFunction();
     });
+
+    it('delegates to addTag method', function() {
+
+    });
+  });
+
+  describe('the getActiveNotes method', function() {
+
+    it('is defined', function() {
+      expect(noteTagService.getActiveNotes).toBeFunction();
+    });
+  });
+
+  describe('the activateTags method', function() {
+
+    it('is defined', function() {
+      expect(noteTagService.activateTags).toBeFunction();
+    });
+
   });
 
 });
