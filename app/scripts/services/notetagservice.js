7d5040512e30a355abf38c7e49cc6996321a9941
Elridge D'Mello <elridge.dmello@drillinginfo.com>
2015-05-20 00:02:42 -0500

    Initial TDD of addTag method

--- app/scripts/services/notetagservice.js
+++ app/scripts/services/notetagservice.js
@@ -11,13 +11,23 @@ angular.module('noteTaggerApp')
   .service('noteTagService', function () {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
-    this.addTag = function(/*noteId, tag*/) {};
+    var noteTagMap = {};
+
+    this.addTag = function(noteId, tag) {
+      var currentTag, currentNotes;
+      currentTag = noteTagMap[tag];
+      if (!currentTag) {
+        noteTagMap[tag] = { notes: [noteId] };
+      }
+    };
 
     this.addTags = function(/*noteId, tags*/) {};
 
     // function to give state
-    this.getAssociatedNotes = function(/*tag*/) {
-
+    this.getAssociatedNotes = function(tag) {
+      if (noteTagMap[tag]) {
+        return noteTagMap[tag].notes;
+      }
     };
 
     this.getActiveNotes = function() {
--- test/spec/services/notetagservice.js
+++ test/spec/services/notetagservice.js
@@ -12,9 +12,26 @@ describe('Service: noteTagService', function () {
   }));
 
   describe('the addTag method', function() {
+    var testTag = 'test';
+
     it('is defined', function() {
       expect(noteTagService.addTag).toBeFunction();
     });
+
+    it('defines a tag if it is new', function() {
+      var noteId = 'note1';
+      var associatedNotesBefore = noteTagService.getAssociatedNotes(testTag);
+      expect(associatedNotesBefore).toBeUndefined();
+
+      // when
+      noteTagService.addTag(noteId, testTag);
+
+      // then
+      expect(noteTagService.getAssociatedNotes(testTag)).toBeArrayOfSize(1);
+      expect(noteTagService.getAssociatedNotes(testTag)[0]).toEqual(noteId);
+    });
+
+    it('adds notes to the tag', function() {});
   });
 
   describe('the addTags method', function() {
