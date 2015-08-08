191ee868b580e9fae19356850cec83de342f7734
Elridge D'Mello <elridge.dmello@drillinginfo.com>
2015-08-07 22:04:18 -0500

    Change indentation to 4 spaces

--- app/scripts/services/notetagservice.js
+++ app/scripts/services/notetagservice.js
@@ -8,66 +8,66 @@
  * Service in the noteTaggerApp.
  */
 angular.module('noteTaggerApp')
-  .service('noteTagService', function () {
-    // AngularJS will instantiate a singleton by calling "new" on this function
+    .service('noteTagService', function () {
+        // AngularJS will instantiate a singleton by calling "new" on this function
 
-    var noteTagMap = {},
-      that = this;
+        var noteTagMap = {},
+            that = this;
 
-    this.addTag = function(noteId, tag) {
-      var currentTag;
-      currentTag = noteTagMap[tag];
-      if (!currentTag) {
-        noteTagMap[tag] = { notes: [noteId] };
-      } else {
-        noteTagMap[tag].notes = noteTagMap[tag].notes.concat(noteId);
-      }
-      noteTagMap[tag].active = true;
-    };
+        this.addTag = function (noteId, tag) {
+            var currentTag;
+            currentTag = noteTagMap[tag];
+            if (!currentTag) {
+                noteTagMap[tag] = {notes: [noteId]};
+            } else {
+                noteTagMap[tag].notes = noteTagMap[tag].notes.concat(noteId);
+            }
+            noteTagMap[tag].active = true;
+        };
 
-    this.addTags = function(noteId, tags) {
-      tags.forEach(function(tag) {
-        that.addTag(noteId, tag);
-      });
-    };
+        this.addTags = function (noteId, tags) {
+            tags.forEach(function (tag) {
+                that.addTag(noteId, tag);
+            });
+        };
 
-    // function to give state
-    this.getAssociatedNotes = function(tag) {
-      if (noteTagMap[tag]) {
-        return noteTagMap[tag].notes;
-      }
-    };
+        // function to give state
+        this.getAssociatedNotes = function (tag) {
+            if (noteTagMap[tag]) {
+                return noteTagMap[tag].notes;
+            }
+        };
 
-    this.getActiveNotes = function() {
-      var activeNotes = [];
-      keys(noteTagMap).forEach(function(tag) {
-        if (noteTagMap[tag].active) {
-          activeNotes = activeNotes.concat(noteTagMap[tag].notes);
+        this.getActiveNotes = function () {
+            var activeNotes = [];
+            keys(noteTagMap).forEach(function (tag) {
+                if (noteTagMap[tag].active) {
+                    activeNotes = activeNotes.concat(noteTagMap[tag].notes);
+                }
+            });
+            return activeNotes;
+        };
+
+        this.activateTags = function (tags) {
+            keys(noteTagMap).forEach(function (tag) {
+                if (!contains(tags, tag)) {
+                    noteTagMap[tag].active = false;
+                }
+            });
+        };
+
+        function contains(array, value) {
+            return array.indexOf(value) !== -1;
         }
-      });
-      return activeNotes;
-    };
 
-    this.activateTags = function(tags) {
-      keys(noteTagMap).forEach(function(tag) {
-        if (!contains(tags, tag)) {
-          noteTagMap[tag].active = false;
+        function keys(object) {
+            var arrayOfKeys = [], key;
+            for (key in object) {
+                if (object.hasOwnProperty(key)) {
+                    arrayOfKeys.push(key);
+                }
+            }
+            return arrayOfKeys;
         }
-      });
-    };
 
-    function contains(array, value) {
-      return array.indexOf(value) !== -1;
-    }
-
-    function keys(object) {
-      var arrayOfKeys = [], key;
-      for(key in object) {
-        if (object.hasOwnProperty(key)) {
-          arrayOfKeys.push(key);
-        }
-      }
-      return arrayOfKeys;
-    }
-
-  });
+    });
--- test/spec/services/notetagservice.js
+++ test/spec/services/notetagservice.js
@@ -2,113 +2,113 @@
 
 describe('Service: noteTagService', function () {
 
-  // load the service's module
-  beforeEach(module('noteTaggerApp'));
+    // load the service's module
+    beforeEach(module('noteTaggerApp'));
 
-  // instantiate service
-  var noteTagService;
-  beforeEach(inject(function (_noteTagService_) {
-    noteTagService = _noteTagService_;
-  }));
+    // instantiate service
+    var noteTagService;
+    beforeEach(inject(function (_noteTagService_) {
+        noteTagService = _noteTagService_;
+    }));
 
-  describe('the addTag method', function() {
-    var testTag = 'test';
+    describe('the addTag method', function () {
+        var testTag = 'test';
 
-    it('is defined', function() {
-      expect(noteTagService.addTag).toBeFunction();
+        it('is defined', function () {
+            expect(noteTagService.addTag).toBeFunction();
+        });
+
+        it('defines a tag if it is new', function () {
+            var noteId = 'note1';
+            var associatedNotesBefore = noteTagService.getAssociatedNotes(testTag);
+            expect(associatedNotesBefore).toBeUndefined();
+
+            // when
+            noteTagService.addTag(noteId, testTag);
+
+            // then
+            expect(noteTagService.getAssociatedNotes(testTag)).toBeArrayOfSize(1);
+            expect(noteTagService.getAssociatedNotes(testTag)[0]).toEqual(noteId);
+        });
+
+        it('adds multiple notes to a tag', function () {
+            var tagBreakfast = 'breakfast',
+                noteQuinoa = 'blueberry_quinoa',
+                noteOmelet = 'colorado_omelette',
+                actual;
+
+            // when
+            noteTagService.addTag(noteOmelet, tagBreakfast);
+            noteTagService.addTag(noteQuinoa, tagBreakfast);
+
+            // then
+            actual = noteTagService.getAssociatedNotes(tagBreakfast);
+            expect(actual).toBeArrayOfSize(2);
+            expect(actual).toContain(noteOmelet);
+            expect(actual).toContain(noteQuinoa);
+        });
     });
 
-    it('defines a tag if it is new', function() {
-      var noteId = 'note1';
-      var associatedNotesBefore = noteTagService.getAssociatedNotes(testTag);
-      expect(associatedNotesBefore).toBeUndefined();
+    describe('the addTags method', function () {
 
-      // when
-      noteTagService.addTag(noteId, testTag);
+        beforeEach(function () {
+            spyOn(noteTagService, 'addTag').and.callThrough();
+        });
 
-      // then
-      expect(noteTagService.getAssociatedNotes(testTag)).toBeArrayOfSize(1);
-      expect(noteTagService.getAssociatedNotes(testTag)[0]).toEqual(noteId);
+        it('is defined', function () {
+            expect(noteTagService.addTags).toBeFunction();
+        });
+
+        it('delegates to addTag method', function () {
+            var noteId = 'breakfast quinoa',
+                tags = ['sweet', 'healthy'];
+
+            // when
+            noteTagService.addTags(noteId, tags);
+
+            // then
+            tags.forEach(function (tag) {
+                expect(noteTagService.addTag)
+                    .toHaveBeenCalledWith(noteId, tag);
+            });
+        });
     });
 
-    it('adds multiple notes to a tag', function() {
-      var tagBreakfast = 'breakfast',
-        noteQuinoa = 'blueberry_quinoa',
-        noteOmelet = 'colorado_omelette',
-        actual;
+    describe('the getActiveNotes method', function () {
 
-      // when
-      noteTagService.addTag(noteOmelet, tagBreakfast);
-      noteTagService.addTag(noteQuinoa, tagBreakfast);
+        it('is defined', function () {
+            expect(noteTagService.getActiveNotes).toBeFunction();
+        });
 
-      // then
-      actual = noteTagService.getAssociatedNotes(tagBreakfast);
-      expect(actual).toBeArrayOfSize(2);
-      expect(actual).toContain(noteOmelet);
-      expect(actual).toContain(noteQuinoa);
-    });
-  });
+        it('newly added tags default to active', function () {
+            noteTagService.addTag('note1', 'tag1');
+            noteTagService.addTag('note2', 'tag2');
 
-  describe('the addTags method', function() {
-
-    beforeEach(function() {
-      spyOn(noteTagService, 'addTag').and.callThrough();
+            // when
+            expect(noteTagService.getActiveNotes()).toBeArrayOfSize(2);
+            expect(noteTagService.getActiveNotes()).toContain('note1');
+            expect(noteTagService.getActiveNotes()).toContain('note2');
+        });
     });
 
-    it('is defined', function() {
-      expect(noteTagService.addTags).toBeFunction();
+    describe('the activateTags method', function () {
+
+        it('is defined', function () {
+            expect(noteTagService.activateTags).toBeFunction();
+        });
+
+        it('is reflected by the notes associated with activated tag', function () {
+            // given
+            noteTagService.addTag('apple_with_pb', 'snack');
+            noteTagService.addTag('chicken_marsala', 'dinner');
+
+            // when
+            noteTagService.activateTags(['snack']);
+
+            // then
+            expect(noteTagService.getActiveNotes()).toEqual(['apple_with_pb']);
+        });
+
     });
-
-    it('delegates to addTag method', function() {
-      var noteId = 'breakfast quinoa',
-        tags = ['sweet', 'healthy'];
-
-      // when
-      noteTagService.addTags(noteId, tags);
-
-      // then
-      tags.forEach(function(tag) {
-        expect(noteTagService.addTag)
-          .toHaveBeenCalledWith(noteId, tag);
-      });
-    });
-  });
-
-  describe('the getActiveNotes method', function() {
-
-    it('is defined', function() {
-      expect(noteTagService.getActiveNotes).toBeFunction();
-    });
-
-    it('newly added tags default to active', function() {
-      noteTagService.addTag('note1', 'tag1');
-      noteTagService.addTag('note2', 'tag2');
-
-      // when
-      expect(noteTagService.getActiveNotes()).toBeArrayOfSize(2);
-      expect(noteTagService.getActiveNotes()).toContain('note1');
-      expect(noteTagService.getActiveNotes()).toContain('note2');
-    });
-  });
-
-  describe('the activateTags method', function() {
-
-    it('is defined', function() {
-      expect(noteTagService.activateTags).toBeFunction();
-    });
-
-    it('is reflected by the notes associated with activated tag', function() {
-      // given
-      noteTagService.addTag('apple_with_pb', 'snack');
-      noteTagService.addTag('chicken_marsala', 'dinner');
-
-      // when
-      noteTagService.activateTags(['snack']);
-
-      // then
-      expect(noteTagService.getActiveNotes()).toEqual(['apple_with_pb']);
-    });
-
-  });
 
 });
